5152058e5b7303d2009741bf4fc465b366537d11
hao <haokidysss@gmail.com>
2020-07-26 04:06:19 -0400

    Iniciando CRUD Funcionarios

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Funcionario
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Ola')
+
+class FuncionariosList(ListView):
+    model = Funcionario
